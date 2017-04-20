1898f1819462593ef58f1d2494bfaf6885251ee2
yuexiriguang <482819895@qq.com>
2017-04-20 13:44:41 +0800

    basic parallize

--- parallize.go
+++ parallize.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+    //"fmt"
     "pkg_wmg"
 )
 
 func main() {
-    pkg_wmg.Read("json.txt")
+    pkg_wmg.Read("json.v2")
     pkg_wmg.InitWafVds()
     pkg_wmg.InitBroker()
     pkg_wmg.UpdateOffset()
     pkg_wmg.InitConsumers()
-    pkg_wmg.InitWafVdsBak()
-    pkg_wmg.ListenHttp() 
- 
+    pkg_wmg.InitCacheInfoMap()
+    go pkg_wmg.Manage()
+    go pkg_wmg.InitPrefetch()
+    pkg_wmg.Listen()
 }
