507794fe2a122f480fd148cf6b08c6286d786bb8
yuexiriguang <482819895@qq.com>
2017-04-23 10:17:44 +0800

    某topic上一次预取消息未得到回应，不会继续向该topic发送预取消息

--- parallize.go
+++ parallize.go
@@ -6,13 +6,15 @@ import (
 )
 
 func main() {
-    pkg_wmg.Read("json.txt")
+    pkg_wmg.Read("json.v2")
     pkg_wmg.InitWafVds()
     pkg_wmg.InitBroker()
     pkg_wmg.UpdateOffset()
     pkg_wmg.InitConsumers()
     pkg_wmg.InitCacheInfoMap()
+    pkg_wmg.InitPrefetchMsgSwitchMap()
     go pkg_wmg.Manage()
     go pkg_wmg.InitPrefetch()
+    go pkg_wmg.Record("record")
     pkg_wmg.Listen()
 }
