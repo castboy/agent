539f037a794ba0399d8eaa0f449a70b6edf3e87e
yuexiriguang <482819895@qq.com>
2017-04-24 12:48:50 +0800

    将agent拆分成waf跟vds两个服务

--- waf.go
+++ waf.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-    pkg_wmg.Read("json.v2")
+    pkg_wmg.Read("waf.record")
     pkg_wmg.InitWafVds()
     pkg_wmg.InitBroker()
     pkg_wmg.UpdateOffset()
@@ -15,6 +15,6 @@ func main() {
     pkg_wmg.InitPrefetchMsgSwitchMap()
     go pkg_wmg.Manage()
     go pkg_wmg.InitPrefetch()
-    go pkg_wmg.Record("record")
-    pkg_wmg.Listen()
+    go pkg_wmg.Record("waf.record")
+    pkg_wmg.Listen("localhost:8081")
 }
