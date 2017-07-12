c5f7a0a17548125447b063a4eac200215d33473d
yuexiriguang <482819895@qq.com>
2017-07-12 10:53:04 +0800

    fix conf.go bug

--- conf.go
+++ conf.go
@@ -3,6 +3,7 @@ package main
 import (
 	"agent_pkg"
 	"encoding/json"
+	"fmt"
 	"strconv"
 
 	"github.com/widuu/goini"
@@ -21,20 +22,22 @@ func SetConf(port int, cache int, partitions map[string]int32, wafTopic string, 
 func main() {
 	conf := goini.SetConfig("conf.ini")
 	confList := conf.ReadList()
+	fmt.Println(confList)
 
-	port, _ := strconv.Atoi(confList[0]["other"]["port"])
-	cache, _ := strconv.Atoi(confList[0]["other"]["cache"])
+	port, _ := strconv.Atoi(confList[7]["other"]["port"])
+	cache, _ := strconv.Atoi(confList[7]["other"]["cache"])
 	wafTopic := confList[0]["onlineTopic"]["waf"]
 	vdsTopic := confList[0]["onlineTopic"]["vds"]
-	endPoint := confList[0]["etcd"]["endPoint"]
+	endPoint := confList[6]["etcd"]["endPoint"]
 
 	var partitions = make(map[string]int32)
-	for key, val := range confList[0]["partition"] {
+	for key, val := range confList[5]["partition"] {
 		partition, _ := strconv.Atoi(val)
 		partitions[key] = int32(partition)
 	}
 
 	setConf := SetConf(port, cache, partitions, wafTopic, vdsTopic)
+	fmt.Println(setConf)
 	agent_pkg.InitEtcdCli(endPoint)
 	agent_pkg.EtcdSet("apt/agent/conf", setConf)
 
