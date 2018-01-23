2f57a7cf2f86d7634867965833a7cb45c8a6f7df
yuexiriguang <482819895@qq.com>
2018-01-23 17:58:41 +0800

    no-hdl

--- agent.go
+++ agent.go
@@ -15,11 +15,9 @@ func main() {
 	Hdfs()
 	go Manage()
 	go InitPrefetch()
-	go SendClearFileHdlMsg(AgentConf.ClearHdfsHdl)
 	CompensationOfflineMsg()
 	go TimingGetOfflineMsg(AgentConf.GetOfflineMsg)
 	go ReqCount()
-	go HdfsClisOffline()
 
 	time.Sleep(time.Duration(AgentConf.DelayListen) * time.Second)
 	Listen()
