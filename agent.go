4084b8733c32284fe0801bc0e2fb111171a6db38
castboy <wangmingguang@byzoro.com>
2018-01-26 15:58:12 +0800

    In addition to the HDFS client reconnect, stable commit.

--- agent.go
+++ agent.go
@@ -3,9 +3,28 @@ package main
 import (
 	. "agent_pkg"
 	"time"
+        "fmt"
+        "github.com/widuu/goini"
+        "strconv"
+        "runtime"
 )
 
+func cpuNum() int {
+    conf := goini.SetConfig("conf.ini")
+    cpu, err := strconv.Atoi(conf.GetValue("other", "cpu"))
+    if nil != err {
+        fmt.Println(err.Error())
+    }
+
+    return cpu
+}
+
 func main() {
+        num := cpuNum()
+        if -1 != num {
+            runtime.GOMAXPROCS(num)
+        }
+
 	InitLog("run/log")
 	GetConf()
 	InitBroker()
@@ -15,10 +34,11 @@ func main() {
 	Hdfs()
 	go Manage()
 	go InitPrefetch()
+        go SendClearFileHdlMsg(AgentConf.ClearHdfsHdl)
 	CompensationOfflineMsg()
 	go TimingGetOfflineMsg(AgentConf.GetOfflineMsg)
 	go ReqCount()
 
-	time.Sleep(time.Duration(AgentConf.DelayListen) * time.Second)
+	time.Sleep(time.Duration(5) * time.Second)
 	Listen()
 }
